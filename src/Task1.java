a5ce4b932b02800f501d157ca7fc5f1e36adb5a7
iaros <iaroslav-stashevsky@yandex.ru>
2023-07-17 17:48:20 +0300

    Initial commit

---
+++ src/Task1.java
@@ -0,0 +1,11 @@
+/**
+ * Задача №1: Мы обязательно изучим Java (База)
+ * Напиши программу, которая выводит на экран надпись: "Мы обязательно изучим Java".
+ */
+
+public class Task1 {
+    public static void main(String[] args) {
+        System.out.println("Мы обязательно изучим Java");
+    }
+
+}
