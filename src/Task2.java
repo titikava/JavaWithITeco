5ba9f0b1deadd1592272345e27d21baf5b670340
iaros <iaroslav-stashevsky@yandex.ru>
2023-07-17 17:50:04 +0300

    -1

---
+++ src/Task2.java
@@ -0,0 +1,11 @@
+/**
+ * Задача №2: Объявляем переменные (База)
+ * Напиши программу, которая в методе main объявляет такие переменные: count типа byte, age типа int и starts типа long.
+ */
+public class Task2 {
+    public static void main(String[] args) {
+        byte count;
+        int age;
+        long stars;
+    }
+}
