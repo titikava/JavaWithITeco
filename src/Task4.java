453809b76d947dae75db136254a7f3fd5703de9a
iaros <iaroslav-stashevsky@yandex.ru>
2023-07-17 17:55:44 +0300

    -1

---
+++ src/Task4.java
@@ -0,0 +1,13 @@
+/**
+ * Задача №4: Диапазоны значений (Со звёздочкой)
+ * Выведите на экран диапазоны значений каждого из целочисленных примитивных типов.
+ */
+
+public class Task4 {
+    public static void main(String[] args) {
+        System.out.println("byte: от " + Byte.MIN_VALUE + " до " + Byte.MAX_VALUE);
+        System.out.println("short: от " + Short.MIN_VALUE + " до " + Short.MAX_VALUE);
+        System.out.println("int: от " + Integer.MIN_VALUE + " до " + Integer.MAX_VALUE);
+        System.out.println("long: от " + Long.MIN_VALUE + " до " + Long.MAX_VALUE);
+    }
+}
