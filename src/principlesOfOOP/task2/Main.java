56094cff76aa6fe91f3416671feaa4b46289edd8
iaros <iaroslav-stashevsky@yandex.ru>
2023-07-24 18:09:00 +0300

    -5

--- src/principlesOfOOP/task2/Main.java
+++ src/principlesOfOOP/task2/Main.java
@@ -1,5 +1,17 @@
 package principlesOfOOP.task2;
 
+/**
+ * Задача №2: Интерфейс Printable (База)
+ * Определить интерфейс Printable, содержащий метод void print().
+ * Определить класс Book, реализующий интерфейс Printable.
+ * Определить класс Magazine, реализующий интерфейс Printable.
+ * Создать массив типа Printable, который будет содержать книги и журналы.
+ * В цикле пройти по массиву и вызвать метод print() для каждого объекта.
+ * Создать статический метод printMagazines(Printable[] printable) в классе Magazine,
+ * который выводит на консоль названия только журналов.
+ * Создать статический метод printBooks(Printable[] printable) в классе Book, который выводит на консоль названия
+ * только книг. Используем оператор instanceof для определения типа.
+ */
 public class Main {
     public static void main(String[] args) {
         Printable[] printables = new Printable[6];
